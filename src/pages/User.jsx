25856b5fed2c39bbe208717dd918f8e0f426cb8a
karan-7058 <aghavkaran164@gmail.com>
2025-11-01 18:51:21 +0530

    added user page for displaying user to the page using dynamic routing and using id params

---
+++ src/pages/User.jsx
@@ -0,0 +1,53 @@
+import { useParams } from "react-router-dom";
+
+export default function User(){
+      const { id } = useParams();
+
+
+      const users = {
+        1: {
+      name: "Karan Aghav",
+      role: "Frontend Developer",
+      location: "Pune, India",
+      linkedin: "https://linkedin.com/in/karanaghav",
+      github: "https://github.com/karan-7058",
+      photo: "https://randomuser.me/api/portraits/men/32.jpg",
+      about:
+        "Passionate about building responsive web apps using React, Tailwind CSS, and modern JavaScript. Focused on clean UI and smooth user experience.",
+      },
+        2: {
+      name: "Priya Sharma",
+      role: "UI/UX Designer",
+      location: "Mumbai, India",
+      linkedin: "https://linkedin.com/in/priyasharma",
+      github: "https://github.com/priyasharma",
+      photo: "https://randomuser.me/api/portraits/women/45.jpg",
+      about:
+        "Creative designer specializing in user experience and digital product design. Loves turning complex problems into elegant visual solutions.",
+      },
+  };
+
+  const user=users[id];
+
+
+  if(!user){
+    return <div>user not found</div>
+  }
+
+    return (
+        <div className="max-w-md mx-auto bg-white shadow-md rounded-md overflow-hidden mt-10">
+             <img src={user.photo} alt={user.name} className="w-full h-48 object-cover" />
+             <div className="p-6">
+                <h2 className="text-xl font-semibold mb-2">{user.name}</h2>
+                <p className="text-gray-600">{user.role}</p>
+                <p className="text-gray-500"> {user.location}</p>
+                <p className="text-gray-400">{user.about}</p>
+                
+
+             </div>
+
+
+        </div>
+      
+    )
+}
