1c3b89dfc492ec3f4fd85b1980d8e1b0189a4113
karan-7058 <aghavkaran164@gmail.com>
2025-11-01 18:53:51 +0530

    add user route to the router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import About from "./pages/About.jsx"
 import Home from "./pages/Home.jsx"
+import User from "./pages/User.jsx"
 import {createBrowserRouter , RouterProvider} from "react-router-dom";
 
 
@@ -13,7 +14,8 @@ const router =createBrowserRouter([
     element:<App/>,
     children:[
        {path:"/" , element:<Home/>},
-       {path:"/about" , element:<About/>}
+       {path:"/about" , element:<About/>},
+       {path:"/users/:id" , element:<User/>}
      
     ]
   },
