1d75a4024a04deb3e170603b88a1b12e274a226b
karan-7058 <aghavkaran164@gmail.com>
2025-11-05 18:17:29 +0530

    tidy imports and mount App in React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {lazy , Suspense} from "react"
-import './index.css'
-import App from './App.jsx'
-const Home=lazy(()=> import("./pages/Home.jsx"))
-const About=lazy(()=>import("./pages/About.jsx"))
-const User=lazy(()=>import("./pages/User.jsx"))
-const Contact=lazy(()=>import("./pages/Contact.jsx"))
-const ErrorPage=lazy(()=>import("./pages/ErrorPage.jsx"))
-import {createBrowserRouter , RouterProvider} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import './App.css'
 
-
-const router =createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>,
-    children:[
-       {path:"/" , element:<Home/>},
-       {path:"/about" , element:<About/>},
-       {path:"/users/:id" , element:<User/>},
-       {path:"/contact" , element:<Contact/>},
-       {path:"*" , element:<ErrorPage/>} 
-     
-    ]
-  },
-
-])
-
-createRoot(document.getElementById('root')).render(
- 
-
-    <Suspense fallback={<p>Loading...</p>}>
-      <RouterProvider router={router} />
-    </Suspense>
-
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />   
+  </React.StrictMode>
+);
