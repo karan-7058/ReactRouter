45df867cfb4c5faa815ecbb644dd7d1c08af833a
karan-7058 <aghavkaran164@gmail.com>
2025-11-06 15:58:34 +0530

    add routerprovider with suspense fallback and mainrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import { RouterProvider } from "react-router-dom"
 import {Suspense} from "react"
-import { RouterProvider } from "react-router-dom";
-import mainRouter from "./routes/mainRouter.jsx";
+import mainRouter from "./routes/mainRouter.jsx"
 
-function App() {
-  return (
-   <Suspense fallback={<p>Loading...</p>}>
-      <RouterProvider router={mainRouter} />
-    </Suspense>
-  )
+export default function App(){
+    return(
+        <Suspense fallback={<div>Loading...</div>}>
+            <RouterProvider router={mainRouter}/>
+        </Suspense>
+       
+    )
 }
-
-export default App;
