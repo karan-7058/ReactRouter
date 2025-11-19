11e90ab171333260a46dcc4bad499e6190724e33
karan-7058 <aghavkaran164@gmail.com>
2025-11-19 14:45:42 +0530

    use the ThemeContext in layout so all components can get the access of theme

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 
 import Navbar from "../component/Navbar.jsx"
 import Footer from "../component/Footer.jsx"
+import { ThemeContext } from "../context/ThemeContext.jsx"
+import {useContext} from "react"
 
 
 import {Outlet} from "react-router-dom"
@@ -9,13 +11,16 @@ import {Outlet} from "react-router-dom"
 import '../App.css'
 
 function MainLayout() {
-  
+  const {theme} =useContext(ThemeContext);
+
   return (
-    <div>
-    <Navbar />
-    <Outlet />
-    <Footer />
-   </div>
+    <div className={theme === "light" ? "bg-white text-black " : "bg-black text-white"}>
+      
+        <Navbar />
+        <Outlet />
+        <Footer />
+      
+    </div>
   )
   
 }
