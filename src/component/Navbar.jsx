650a4d1f806becc88279d274ab9694db5c08b4d7
karan-7058 <aghavkaran164@gmail.com>
2025-11-19 14:49:46 +0530

    extract the theme and toggleTheme values from themeContext and use it in button for styling the theme add the toggle theme button to the navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,24 @@
-import {Link , NavLink , useNavigate , useLocation} from "react-router-dom";
+import {NavLink , useNavigate , useLocation} from "react-router-dom";
+import {ThemeContext} from "../context/ThemeContext.jsx";
+import {useContext} from "react";
+
 
 export default function Navbar() {
   const navigate=useNavigate();
   const location=useLocation();
+  const {theme , toggleTheme}=useContext(ThemeContext);
+
+
 
   return (
     <nav className="bg-gray-900 text-white flex justify-between items-center px-6 py-3 shadow-lg">
       <h1 className="text-2xl font-semibold cursor-pointer" onClick={()=>navigate("/")}>Wanderer Stay</h1>
       <div className="space-x-6">
+
+        <button onClick={toggleTheme} className={`px-4 py-2 rounded ${theme === "light" ? "bg-white text-black" : "bg-black text-white"}`}>
+          Toggle Theme
+        </button>
+
         <NavLink
           to="/"
           className={({ isActive }) =>
