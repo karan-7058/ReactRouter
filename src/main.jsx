62b67695ed22dfa50e959c365101f112b242769c
karan-7058 <aghavkaran164@gmail.com>
2025-11-19 14:53:01 +0530

    add all the theme logic to the app using themeprovider function

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import './App.css'
+import {ThemeProvider} from "./context/ThemeContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
+
   <React.StrictMode>
-    <App />   
+    <ThemeProvider>
+    <App />  
+    </ThemeProvider> 
+    
   </React.StrictMode>
 );
